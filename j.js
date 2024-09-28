2941ac0eefc420e9c2be90cdb612ef6500e47af1
prathamchavhan <chavhanpratham06@gmail.com>
2024-09-28 14:01:34 +0530

    Add files via upload

---
+++ j.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent! Thank you for contacting me.');
+    this.reset();
+});
